634344e2d4684ab0d43541ae023e153df4171ebc
Jeff Thomas <jeff.thomas137@gmail.com>
2017-02-19 16:27:23 -0500

    Adding jupyter notebook and collect_stats that collect offensive Rebounds for a range of games--will need Robot Atmn

--- collect_stats.py
+++ collect_stats.py
@@ -7,16 +7,24 @@ game_id = '0021600849'
 tID = '1610612738'
 
 bs = game.Boxscore(game_id=game_id, season='2016-17', season_type='Regular Season', range_type='0', start_period='0', end_period='0', start_range='0', end_range='0')
-print bs.player_stats()
+df_total = bs.player_stats()
+df_total = df_total[df_total['TEAM_CITY']=='Boston'.encode('utf-8')]
+df_total.fillna(value=0)
 # bsA = game.BoxscoreAdvanced(game_id=game_id, season='2016-17', season_type='Regular Season', range_type='0', start_period='0', end_period='0', start_range='0', end_range='0')
 # print bsA.sql_players_advanced()
+
+noGames = 5
+for x in range(0,noGames+1):
+	bs = game.Boxscore(game_id=game_id, season='2016-17', season_type='Regular Season', range_type='0', start_period='0', end_period='0', start_range='0', end_range='0')
+	df = bs.player_stats()
+	df = df[df['TEAM_CITY']=='Boston'.encode('utf-8')]
+	df.fillna(value=0)
+	oreb_gameRange = pd.concat([df_total,df]).groupby(['PLAYER_NAME'], as_index=False)['OREB'].sum()
+
 
 # tDetails = team.TeamDetails(tID)
 # tSeasons = team.TeamSeasons(tID, league_id='00', season_type='Regular Season', per_mode='PerGame')
 # print tDetails.awards_championships()
 # print tSeasons.info()
 
-columns = ['A','B']
-df = pd.DataFrame(index=range(0,4), columns = columns)
-df = df.fillna(0)
-print df
+print oreb_gameRange
